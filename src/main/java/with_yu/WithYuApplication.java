be1590c0bbc02b4964393c9290aca385a3964bad
hky035 <rldud035@naver.com>
2024-08-23 21:43:44 +0900

    init: init project

---
+++ src/main/java/with_yu/WithYuApplication.java
@@ -0,0 +1,13 @@
+package with_yu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithYuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithYuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/with_yu/WithYuApplicationTests.java
@@ -0,0 +1,13 @@
+package with_yu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithYuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
