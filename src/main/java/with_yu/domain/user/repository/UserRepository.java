1be7716ec448b7d4b8d318d6c28385169eef9850
hky035 <rldud035@naver.com>
2024-10-08 02:01:03 +0900

    feat: 학번 중복 방지를 위한 조회 메서드 추가

--- src/main/java/with_yu/domain/user/repository/UserRepository.java
+++ src/main/java/with_yu/domain/user/repository/UserRepository.java
@@ -3,8 +3,12 @@ package with_yu.domain.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import with_yu.domain.user.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
     boolean existsByPhoneNumber(String phoneNumber);
     boolean existsByNickname(String nickname);
+    boolean existsByStudentNumber(int studentNumber);
 }
