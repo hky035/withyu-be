5d87570e23841a1b8a56e25ecc5fefc0ccb12ceb
hky035 <rldud035@naver.com>
2024-10-07 20:41:12 +0900

    feat: BcryptPasswordEncoder 빈 등록

---
+++ src/main/java/with_yu/config/SecurityAuthConfig.java
@@ -0,0 +1,19 @@
+package with_yu.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityAuthConfig {
+
+    @Bean
+    public PasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
