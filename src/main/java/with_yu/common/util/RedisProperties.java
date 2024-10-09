dc4307b088997cf52d277f1da3d4f27663f56f05
hky035 <rldud035@naver.com>
2024-10-10 01:29:08 +0900

    feat: 이메일 인증 코드 Redis 저장 키 prefix 설정

--- src/main/java/with_yu/common/util/RedisProperties.java
+++ src/main/java/with_yu/common/util/RedisProperties.java
@@ -3,4 +3,5 @@ package with_yu.common.util;
 public interface RedisProperties {
     String REFRESH_TOKEN_PREFIX = "refresh-token::";
     String BLACKLIST_TOKEN_PREFIX = "blacklist-token::";
+    String CODE_PREFIX = "code::";
 }
