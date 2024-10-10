0bb7b0eb0ceff82c12342f0d3af371380c77998a
hky035 <rldud035@naver.com>
2024-10-10 17:30:05 +0900

    feat: 인증 코드 만료기한 설정

--- src/main/java/with_yu/common/util/RedisProperties.java
+++ src/main/java/with_yu/common/util/RedisProperties.java
@@ -4,4 +4,5 @@ public interface RedisProperties {
     String REFRESH_TOKEN_PREFIX = "refresh-token::";
     String BLACKLIST_TOKEN_PREFIX = "blacklist-token::";
     String CODE_PREFIX = "code::";
+    Long CODE_EXPIRATION = 180000L;
 }
